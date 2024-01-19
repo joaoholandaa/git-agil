20349425efcb9a65e03c27c60998ee8730d93616
Joao Holanda <joaocmdt505@gmail.com>
2024-01-19 17:03:38 -0300

    feat: Adding new messages to project

--- messages.py
+++ messages.py
@@ -1,5 +1,6 @@
 display_messages = [
   'Seja Feliz :)',
   'Fique tranquilo, tudo vai acabar bem!',
-  'Olá Mundo! Estou aqui!'
+  'Olá Mundo! Estou aqui!',
+  'Acredite nos seus caminhos! Sempre!'
 ]
