e159753f33b4b9d1da300f34a28064a4b08c0bd2
Joao Holanda <joaocmdt505@gmail.com>
2024-01-24 15:48:47 -0300

    feat: Implementing login

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ import time
 print('Iniciando o Projeto 3..2..1..')
 time.sleep(3)
 
+login_username = [
+    'Maria',
+    'Joao',
+    'Pedro',
+    'Lucas',
+    'Marcos',
+    'Paulo'
+]
+
+username = input('usuario para login: ')
+
 while True:
+    while(username not in login_username):
+        print()
+        print('Usario Invalido! Tente novamente!')
+        username = input('usuario para login: ')
+
     resposta = input('Deseja receber um conselho? S/N: ')
     if (resposta == 'S' or resposta == 's'):
+        print()
         messagem = random.choice(display_messages)
         print(messagem)
         print()
