ac85284bb1cf825c9c15263a376448461b2ebe64
Joao Holanda <joaocmdt505@gmail.com>
2024-01-24 14:23:26 -0300

    feat: Implementing some new things

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ print('Iniciando o Projeto 3..2..1..')
 time.sleep(3)
 
 while True:
-  resposta = input('Deseja receber um conselho? S/N: ')
-  if (resposta == 'S' or resposta == 's'):
-    messagem = random.choice(display_messages)
-    print(messagem)
-    print()
+    resposta = input('Deseja receber um conselho? S/N: ')
+    if (resposta == 'S' or resposta == 's'):
+        messagem = random.choice(display_messages)
+        print(messagem)
+        print()
--- messages.py
+++ messages.py
@@ -1,8 +1,8 @@
 display_messages = [
-  'Seja Feliz :)',
-  'Fique tranquilo, tudo vai acabar bem!',
-  'Olá Mundo! Estou aqui!',
-  'Acredite nos seus caminhos! Sempre!'
-  'Sempre apague as luzes antes de sair!',
-  'Estamos aqui aprendendo sobre Branches'
+    'Seja Feliz :)',
+    'Fique tranquilo, tudo vai acabar bem!',
+    'Olá Mundo! Estou aqui!',
+    'Acredite nos seus caminhos! Sempre!',
+    'Sempre apague as luzes antes de sair!',
+    'Estamos aqui aprendendo sobre Branches'
 ]
