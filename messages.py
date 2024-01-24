8235716e7fafbe20834cfcbe9df27aa775581968
Joao Holanda <joaocmdt505@gmail.com>
2024-01-24 14:37:54 -0300

    test: Implementing first test

--- messages.py
+++ messages.py
@@ -2,7 +2,7 @@ display_messages = [
   'Seja Feliz :)',
   'Fique tranquilo, tudo vai acabar bem!',
   'Olá Mundo! Estou aqui!',
-  'Acredite nos seus caminhos! Sempre!'
+  'Acredite nos seus caminhos! Sempre!',
   'Sempre apague as luzes antes de sair!',
   'Estamos aqui aprendendo sobre Branches'
 ]
